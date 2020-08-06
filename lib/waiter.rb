33d1f1e5bf701067aa1eb4ba3bfc621ba1167dee
Andy Adams <andrewdcadams@gmail.com>
2020-08-06 22:22:14 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,21 @@
 class Waiter
-
+  
+  attr_reader :age 
+  attr_accessor :years_of_experience, :customer
+  
+  @@all = []
+  
+  def initialize(age,years_of_experience)
+    @age = age 
+    @years_of_experience = years_of_experience
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  
+  
+  
 end
