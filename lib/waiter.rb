6583892691f868405ed95048e5773a3aa7336efb
Andy Adams <andrewdcadams@gmail.com>
2020-08-06 22:30:26 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -19,7 +19,16 @@ class Waiter
    Meal.new(self, customer, total, tip)
   end
   
+  def meals
+    Meal.all.select {|meal| meal.waiter == self }
+  end
   
+  def waiters
+    x = meals
+    y = []
+    x.each {|xmeal| y << xmeal.waiter}
+    y 
+  end
   
   
 end
